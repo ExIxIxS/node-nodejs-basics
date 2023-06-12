bea6ad964ace55133ab580d789a4643e969caec8
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-06-12 12:46:58 +0200

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import { open, close, appendFile } from 'node:fs';
+
+function closeFd(fd) {
+  close(fd, (err) => {
+    if (err) throw err;
+  });
+}
+
 const create = async () => {
-    // Write your code here 
+    open('./src/fs/files/fresh.txt', 'wx', (err, fd) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                throw new Error('FS operation failed');
+            }
+        }
+
+        try {
+            appendFile(fd, 'I am fresh and young', 'utf8', (err) => {
+            if (err) throw err;
+            });
+        } catch(err) {
+            throw err;
+        } finally {
+            closeFd(fd);
+        }
+    });
+
 };
 
 await create();
