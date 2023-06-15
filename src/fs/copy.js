c5258fe40c0b881baaca44747856a0207196fb55
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-06-15 12:54:18 +0200

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import { existsSync, cp } from 'node:fs';
+import path from 'node:path';
+
+/*
+copy.js - implement function that copies folder files files with all its content into folder files_copy at the same level
+    (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)
+*/
+
+const SOURSE_PATH = './src/fs/files';
+const TARGET_PATH = './src/fs/files_copy';
+const absoluteSourcePath = path.resolve(SOURSE_PATH);
+const absoluteDestinationPath = path.resolve(TARGET_PATH);
+
 const copy = async () => {
-    // Write your code here 
+    const isSourseExists = existsSync(SOURSE_PATH);
+    const isTargetExists = existsSync(TARGET_PATH);
+
+    if (!isSourseExists || isTargetExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        cp( absoluteSourcePath,
+            absoluteDestinationPath,
+            {recursive: true, force: false, errorOnExist: true},
+            () => null,
+        )
+      } catch (err) {
+        throw new Error('FS operation failed');
+      }
 };
 
 await copy();
