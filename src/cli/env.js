119446d7be3507112059a4be9e1c65aac40aecf1
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-06-15 14:58:21 +0200

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+
+/*
+env.js - implement function that parses environment variables with prefix RSS_
+    and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+*/
+
+const PREFIX = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    Object
+        .entries(process.env)
+        .filter(([key, _]) => key.slice(0, PREFIX.length) === PREFIX)
+        .forEach(([key, value]) => console.log(`${key}=${value}`));
 };
 
 parseEnv();
