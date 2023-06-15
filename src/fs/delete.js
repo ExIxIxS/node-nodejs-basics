4174e67841fc70a9b31a6b3f26e67eba58108836
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-06-15 14:22:15 +0200

    feat: add read.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -5,7 +5,7 @@ delete.js - implement function that deletes file fileToRemove.txt
     (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
 */
 
-const TARGET = './src/fs/files/fileToRemove.';
+const TARGET = './src/fs/files/fileToRemove.txt';
 
 const remove = async () => {
     rm(TARGET, callback)
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { readFile } from 'node:fs';
+/*
+read.js - implement function that prints content of the fileToRead.txt into console
+    (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)
+*/
+
+const TARGET = './src/fs/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    readFile(TARGET, 'utf8', callback);
 };
+
+function callback(err, text) {
+    if (err) {
+        throw new Error('FS operation failed');
+    }
+
+    console.log(text);
+}
 
 await read();
