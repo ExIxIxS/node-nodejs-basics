9a90770ebd8dfec4a0df2d8f73202a20e22abcf8
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-06-19 12:19:56 +0200

    feat: add cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,30 @@
+import { spawn } from 'child_process';
+
+/*
+cp.js - implement function spawnChildProcess that receives array of arguments args and creates child process from file script.js,
+        passing these args to it. This function should create IPC-channel between stdin and stdout of master process and child process:
+        child process stdin should receive input from master process stdin
+        child process stdout should send data to master process stdout
+*/
+
+const CHILD_PATH = './src/cp/files/script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  return new Promise((resolve, reject) => {
+    const childProcess = spawn('node', [CHILD_PATH, ...args]);
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+
+    childProcess.on('exit', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Child process exited with code ${code}`));
+      }
+    });
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const testArr = [1, 2, 3, 'Go!!!'] // Put your arguments to test this functionality
+
+spawnChildProcess(testArr);
